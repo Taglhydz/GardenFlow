508250e71b5caa40c7e332eef660008d7d4221e4
Tom Vaillant <tom.vaillant.vt@gmail.com>
2025-10-19 16:25:11 +0200

    create garden model

--- backend/src/models/gardenModel.js
+++ backend/src/models/gardenModel.js
@@ -1,7 +1,59 @@
 const db = require('../database/db');
 
 const Garden = {
-  // TODO: getAll, getById, getByUserId, create, update, softDelete
+  getAll: () => {
+    return new Promise((resolve, reject) => {
+      db.query('SELECT * FROM garden WHERE deleted_at IS NULL', (err, results) => {
+        if (err) return reject(err);
+        resolve(results);
+      });
+    });
+  },
+
+  getById: (id) => {
+    return new Promise((resolve, reject) => {
+      db.query('SELECT * FROM garden WHERE id = ? AND deleted_at IS NULL', [id], (err, results) => {
+        if (err) return reject(err);
+        resolve(results[0]);
+      });
+    });
+  },
+
+  getByUserId: (user_id) => {
+    return new Promise((resolve, reject) => {
+      db.query('SELECT * FROM garden WHERE user_id = ? AND deleted_at IS NULL', [user_id], (err, results) => {
+        if (err) return reject(err);
+        resolve(results);
+      });
+    });
+  },
+
+  create: (garden) => {
+    return new Promise((resolve, reject) => {
+      db.query('INSERT INTO garden (user_id, name, location, description) VALUES (?, ?, ?, ?)', [garden.user_id, garden.name, garden.location, garden.description], (err, result) => {
+        if (err) return reject(err);
+        resolve({ id: result.insertId, ...garden });
+      });
+    });
+  },
+
+  update: (id, garden) => {
+    return new Promise((resolve, reject) => {
+      db.query('UPDATE garden SET name = ?, location = ?, description = ? WHERE id = ? AND deleted_at IS NULL', [garden.name, garden.location, garden.description, id], (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      });
+    });
+  },
+
+  softDelete: (id) => {
+    return new Promise((resolve, reject) => {
+      db.query('UPDATE garden SET deleted_at = NOW() WHERE id = ?', [id], (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      });
+    });
+  },
 };
 
 module.exports = Garden;
